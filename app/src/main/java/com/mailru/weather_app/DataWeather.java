d623d11dd97511f6fc3cac431362db7f025d0c07
Punk_Off <pankov.kirill1997@yandex.ru>
2020-04-05 17:20:31 +0300

    Выход в интернет. Доделать сохранение подсветки города при повороте и формат времени.

--- app/src/main/java/com/mailru/weather_app/DataWeather.java
+++ app/src/main/java/com/mailru/weather_app/DataWeather.java
@@ -1,14 +1,12 @@
 package com.mailru.weather_app;
 
-import android.graphics.drawable.Drawable;
-
 public class DataWeather {
 
     private String day;
-    private Drawable img;
+    private String img;
     private String grad;
 
-    public DataWeather(String day, Drawable img, String grad) {
+    public DataWeather(String day, String img, String grad) {
         this.day = day;
         this.img = img;
         this.grad = grad;
@@ -18,7 +16,7 @@ public class DataWeather {
         return day;
     }
 
-    Drawable getImg() {
+    String getImg() {
         return img;
     }
 
--- app/src/main/java/com/mailru/weather_app/RecyclerWeekendAdapter.java
+++ app/src/main/java/com/mailru/weather_app/RecyclerWeekendAdapter.java
@@ -1,9 +1,9 @@
 package com.mailru.weather_app;
 
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,23 +13,25 @@ import java.util.ArrayList;
 
 public class RecyclerWeekendAdapter extends RecyclerView.Adapter<RecyclerWeekendAdapter.MyViewHolder> {
 
+    private Typeface weatherFont;
     private ArrayList<DataWeather> dataWeathers;
 
-    public RecyclerWeekendAdapter(ArrayList<DataWeather> dataWeathers) {
+    public RecyclerWeekendAdapter(ArrayList<DataWeather> dataWeathers, Typeface weatherFont) {
         this.dataWeathers = dataWeathers;
+        this.weatherFont = weatherFont;
     }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.week_weather_item, parent, false);
-        return new MyViewHolder(view);
+        return new MyViewHolder(view, weatherFont);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         holder.textViewDay.setText(dataWeathers.get(position).getDay());
-        holder.imageView.setImageDrawable(dataWeathers.get(position).getImg());
+        holder.imageView.setText(dataWeathers.get(position).getImg());
         holder.textViewGrad.setText(dataWeathers.get(position).getGrad());
     }
 
@@ -39,16 +41,23 @@ public class RecyclerWeekendAdapter extends RecyclerView.Adapter<RecyclerWeekend
     }
 
     static class MyViewHolder extends RecyclerView.ViewHolder {
-
         TextView textViewDay;
-        ImageView imageView;
+        TextView imageView;
         TextView textViewGrad;
+        Typeface weatherFont;
 
-        MyViewHolder(@NonNull View itemView) {
+        MyViewHolder(@NonNull View itemView, Typeface weatherFont) {
             super(itemView);
             textViewDay = itemView.findViewById(R.id.weekday);
             imageView = itemView.findViewById(R.id.imageView);
             textViewGrad = itemView.findViewById(R.id.gradView);
+            this.weatherFont = weatherFont;
+            initFonts();
         }
+
+        private void initFonts() {
+            imageView.setTypeface(weatherFont);
+        }
+
     }
 }
